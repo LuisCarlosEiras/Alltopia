261bc2e196bf84e938d4b5c8bb92012cedd4b9e3
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2024-08-04 15:48:37 -0300

    Update alltopia.py

--- alltopia.py
+++ alltopia.py
@@ -39,7 +39,7 @@ st.markdown("""
         # background-color: #f0f8ff;
     }
     div.stButton > button:first-child {
-        background-color: #0066cc;
+        # background-color: #0066cc;
         color: white;
         font-size: 20px;
         font-weight: bold;
