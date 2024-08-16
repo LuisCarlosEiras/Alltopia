757b9aa8ca411b304d6190e4bd7668c19d464111
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2024-08-16 15:02:19 -0300

    Update alltopia.py

--- alltopia.py
+++ alltopia.py
@@ -1,7 +1,4 @@
-
 import streamlit as st
-st.set_option('deprecation.showfileUploaderEncoding', False)
-st.set_option('runner.catch_log_msg', True)
 import cv2
 import numpy as np
 from transformers import LLaMAForSequenceClassification, LLaMATokenizer
