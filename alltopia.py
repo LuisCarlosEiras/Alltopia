6d90947deb81dd4a692cd2981b2386fd2b4fe797
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2024-08-18 14:50:49 -0300

    Update alltopia.py

--- alltopia.py
+++ alltopia.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from camera_input_live import camera_input_live
+image = camera_input_live()
+st.image(value)
+
 from langchain import LLMChain, PromptTemplate
 from langchain.memory import SimpleMemory
 from langchain_groq import ChatGroq
