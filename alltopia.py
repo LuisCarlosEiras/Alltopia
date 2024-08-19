a5ae0e2be40968cc41e68fc8ed46903a83d17513
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2024-08-19 12:27:05 -0300

    Update alltopia.py

--- alltopia.py
+++ alltopia.py
@@ -20,6 +20,7 @@ except Exception as e:
 
 def process_image(image, max_size=(800, 800), quality=85):
     img = Image.open(image)
+    img = img.convert('RGB')  # Converte a imagem para RGB
     img.thumbnail(max_size)
     buf = io.BytesIO()
     img.save(buf, format='JPEG', quality=quality)
