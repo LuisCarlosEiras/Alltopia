8047301369ff5655c1b9d4475f1c3b813a3ccc2b
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2024-08-22 08:35:10 -0300

    Update alltopia.py

--- alltopia.py
+++ alltopia.py
@@ -17,9 +17,9 @@ if image_data is not None:
     # Exibe a imagem capturada
     st.image(image, caption="Imagem Capturada", use_column_width=True)
     
-    # Exibe informações adicionais da imagem (opcional)
+    # Exibe informações adicionais da imagem
     st.write(f"Tipo da imagem capturada: {type(image)}")
-    st.write(f"Dimensões da imagem: {image.size}")  # Agora acessamos as dimensões da imagem PIL
+    st.write(f"Dimensões da imagem: {image.size}")  # Acesso às dimensões após conversão para PIL Image
     
     # Se a imagem for capturada, você pode prosseguir para enviar a imagem para a API Groq
     if st.button("Enviar Imagem para Groq API"):
