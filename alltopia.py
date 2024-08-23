098cb6d716df32015cd9ede3fe529b0b7ff2e60b
Luís Carlos Silva Eiras <luiscarloseiras@gmail.com>
2024-08-23 08:54:49 -0300

    Update alltopia.py

--- alltopia.py
+++ alltopia.py
@@ -1,28 +1,10 @@
+# !pip install streamlit-camera-input-live
+
 import streamlit as st
+
 from camera_input_live import camera_input_live
-from PIL import Image
-import io
 
-# Interface do Streamlit
-st.title("Captura de Imagens com Streamlit")
+image = camera_input_live()
 
-# Captura de imagem com streamlit-camera-input-live
-image_data = camera_input_live()
-
-# Verificação se a imagem foi capturada e exibição
-if image_data is not None:
-    # Converte o objeto BytesIO em uma imagem PIL
-    image = Image.open(io.BytesIO(image_data.read()))
-    
-    # Exibe a imagem capturada
-    st.image(image, caption="Imagem Capturada", use_column_width=True)
-    
-    # Exibe informações adicionais da imagem
-    st.write(f"Tipo da imagem capturada: {type(image)}")
-    st.write(f"Dimensões da imagem: {image.size}")  # Acesso às dimensões após conversão para PIL Image
-    
-    # Se a imagem for capturada, você pode prosseguir para enviar a imagem para a API Groq
-    if st.button("Enviar Imagem para Groq API"):
-        st.write("A implementação do envio da imagem para a API Groq seria adicionada aqui.")
-else:
-    st.error("Nenhuma imagem capturada.")
+if image:
+  st.image(image)
